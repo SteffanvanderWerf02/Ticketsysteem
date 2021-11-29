4e16add211e055ef65bb29a0ff9c1897f0678f86
Jesse Vugteveen <jesse.vugteveen@student.nhlstenden.com>
2021-11-29 16:48:28 +0100

    begin gemaakt met overzichtpagina

---
+++ overzicht.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Overzicht</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class=".col-lg-12">hello</div>
+        </div>
+            
+
+    </div>
+</body>
+</html>
