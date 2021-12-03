4a95eeba8470223f53ae86cde013b6ef668d1f82
SteffanvanderWerf02 <steffan.van.der.werf@student.nhlstenden.com>
2021-12-03 13:06:04 +0100

    ticket overview css fix

--- pages/ticket_overview.php
+++ pages/ticket_overview.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="nl">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <?php include_once("../components/head.html")?>
     <title>Bottom up - Overzicht</title>
 </head>
 
 <body>
+    <!-- Header include -->
+    <?php include_once("../components/header.html")?>
     <div class="container">
         <h1>Tickets</h1>
         <div class="row">
@@ -100,6 +99,8 @@
             </div>
         </div>
     </div>
+     <!-- Footer include -->
+     <?php include_once("../components/footer.html")?>
 </body>
 
 </html>
