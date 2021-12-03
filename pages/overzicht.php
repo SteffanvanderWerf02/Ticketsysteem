5c43b7c203eb11d237164a1d086aff11a9c1c69c
DelanoKoster1 <delano.koster@student.nhlstenden.com>
2021-12-03 15:48:17 +0100

    profiel pagina aangemaakt

--- pages/overzicht.php
+++ pages/overzicht.php
@@ -1,38 +1,37 @@
 <!DOCTYPE html>
 <html lang="en" class="h-100">
 <head>
-    <?php include_once("../components/head.html")?>
-    <title>Bottom up - voorpagina</title>
-    <title>Overzicht</title>
+    <?php include_once("../components/head.html") ?>
+    <title>Bottom up - Overzicht</title>
 </head>
 <body class="h-100">
-    <!--Header include -->
-    <?php include_once("../components/header.html")?>
-    <div class="container h-100">
-        <div class="row h-100">
-            <div class="col-lg-3 my-auto">
-                <div class="card square mx-auto justify-content-center">
-                    <a href="#" class="fill-div">mijn account</a>
-                </div>
+<!--Header include -->
+<?php include_once("../components/header.html") ?>
+<div class="container h-100">
+    <div class="row h-100">
+        <div class="col-lg-3 my-auto">
+            <div class="card square mx-auto justify-content-center">
+                <a href="#" class="fill-div">mijn account</a>
             </div>
-            <div class="col-lg-3 my-auto">
-                <div class="card square mx-auto justify-content-center">
-                    <a href="#" class="fill-div">ticket toevoegen</a>
-                </div>
+        </div>
+        <div class="col-lg-3 my-auto">
+            <div class="card square mx-auto justify-content-center">
+                <a href="#" class="fill-div">ticket toevoegen</a>
             </div>
-            <div class="col-lg-3 my-auto">
-                <div class="card square mx-auto justify-content-center">
-                    <a href="#" class="fill-div">Service / diensten aanvragen</a>
-                </div>
+        </div>
+        <div class="col-lg-3 my-auto">
+            <div class="card square mx-auto justify-content-center">
+                <a href="#" class="fill-div">Service / diensten aanvragen</a>
             </div>
-            <div class="col-lg-3 my-auto">
-                <div class="card square mx-auto justify-content-center">
-                   <a href="#" class="fill-div">mijn account</a>
-                </div> 
+        </div>
+        <div class="col-lg-3 my-auto">
+            <div class="card square mx-auto justify-content-center">
+                <a href="#" class="fill-div">mijn account</a>
             </div>
         </div>
     </div>
-    <!-- Footer include -->
-    <?php include_once("../components/footer.html")?>
+</div>
+<!-- Footer include -->
+<?php include_once("../components/footer.html") ?>
 </body>
 </html>
