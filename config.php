9399810c6b61aae58fec7ecba0b09d36e5c88d9c
DelanoKoster1 <delano.koster@student.nhlstenden.com>
2021-12-07 15:05:10 +0100

    informatie laten uitlezen uit de database in de forms

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// Project variables
+define('PROJECT_PATH','Opdrachten\ProjectP2\Ticketsysteem/');
+define('ROOT_PATH', dirname(__DIR__) . '/');
+define("MAIL_HEADERS", "MIME-Version: 1.0" . "\r\n"."Content-type:text/html;charset=UTF-8" . "\r\n");
+
+// database variables
+define('CONNECTION','localhost');
+define('DATABASE','bottomup');
+define('USERNAME','root');
+define('PASSWORD','');
