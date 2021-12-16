6fbcdb42cdab19ea9d2181f71ffb950eb77655b9
robin <robenjay@hotmail.nl>
2021-12-16 12:47:24 +0100

    filter function for overview

--- components/functions.php
+++ components/functions.php
@@ -5,4 +5,126 @@ function printArray($array)
     print_r($array);
     echo '</pre>';
 }
+
+function getIssueOverview($db, $companyId, $userId, $issueType, $filterStatus, $searchId, $searchTitle) {
+    $type = "s";
+    $params = array($issueType);
+
+    $return = '
+        <table cellspacing="0" cellpadding="0" class="table">
+        <thead>
+            <tr data-href="ticket_detail.php?id=1">
+                <th>id</th>
+                <th>Aanmaak datum</th>
+                <th>Titel</th>
+                ';
+
+    ($companyId == NULL) ? ''  : $return .= '<th>Bedrijf</th><th>Herhalen</th>';
+
+    $return .= '<th>Prioriteit</th>
+                <th>Status</th>
+                <th>Hoofdcategorie</th>
+                <th>Categorie</th>
+            </tr>
+        </thead>
+    <tbody>
+    ';
+    $query= "
+        SELECT  issue.issue_id,
+                issue.created_at,
+                issue.title,
+                issue.priority,
+                issue.`status`,
+                issue.category,
+                issue.frequency,
+                issue.sub_category, 
+                company.name
+        FROM    issue
+        LEFT JOIN company 
+        ON issue.company_id = company.company_id
+        WHERE issue.category = ?
+    ";
+    // ($companyId == NULL) ? $query.=" AND issue.user_id = ? " : $query.=" AND issue.company_id = ?";  
+    switch ($companyId) {
+        case NULL:
+            $query.=" AND issue.user_id = ? ";
+            $type .= "i";
+            array_push($params, $userId);
+            break;
+        default: 
+            $query.=" AND issue.company_id = ?";
+            $type .= "i";
+            array_push($params, $companyId);
+    }
+
+    switch ($filterStatus) {
+        case '1':
+            $query.= " AND issue.status = 1";
+            break;
+        case '2':
+            $query.= " AND issue.status = 2";
+            break;
+        case '3':
+            $query.= " AND issue.status = 3";
+            break;
+        case '4':
+            $query.= " AND issue.status = 4";
+            break;
+    }
+
+    if (!empty($searchId) && empty($searchTitle)) {
+        $query .= " AND issue.issue_id = ?";
+        $type .= "i";
+        array_push($params, $searchId);
+    } elseif (!empty($searchTitle) && empty($searchId)) {
+        $query .= " AND issue.title LIKE '?%'";
+        $type .= "s";
+        array_push($params, $searchTitle);
+       
+    } else {
+        //return "you can't filter on both at the same time";
+        $query .= " AND issue.title LIKE '?%'";
+        $query .= " AND issue.issue_id = ?";
+        $type .= "si";
+        array_push($params, $searchTitle, $searchId);
+    }
+
+     $stmt = mysqli_prepare($db,$query);
+        // return var_dump($params) . $query;
+        // return $query;
+        call_user_func_array(array($stmt, "bind_param"), makeValuesReferenced(array_merge(array($type), $params)));
+        mysqli_stmt_execute($stmt) OR DIE(mysqli_error($db));
+        mysqli_stmt_store_result($stmt) OR DIE(mysqli_error($db));
+        mysqli_stmt_bind_result($stmt, $issueID, $createdAt, $title, $priority, $status, $category, $frequency, $subCategory, $companyName);
+        if(mysqli_stmt_num_rows($stmt) > 0) {
+            while(mysqli_stmt_fetch($stmt)) {
+                    
+                $return .= "<tr class='action' data-href='ticket_detail.php?id={$issueID}'>
+                    <td>{$issueID}</td>
+                    <td>{$createdAt}</td>
+                    <td>{$title}</td>";
+                    ($companyId == NULL) ? ""  : $return .= "<td>{$companyName}</td> <td>{$frequency}</td> ";
+                    
+                $return .= "
+                    <td>{$priority}</td>
+                    <td>{$status}</td>
+                    <td>{$category}</td>
+                    <td>{$subCategory}</td>
+                </tr>";
+            }
+        } else {
+            $return .= "<td colspan='8'>U heeft momenteel geen nieuwe dienst/service aanvragen.</td>";
+        }                  
+        $return .= "</tbody></table>";
+        
+        return $return;
+    }
+    //this function makes from merged to seperate and individual values for the bind param function
+    function makeValuesReferenced($arr){
+        $refs = array();
+        foreach($arr as $key => $value)
+            $refs[$key] = &$arr[$key];
+        return $refs;
+    
+    }
 ?>
--- pages/diensten_overview.php
+++ pages/diensten_overview.php
@@ -1,10 +1,27 @@
-<?php include_once("../config.php");?>
-<?php include_once("../connection.php");?>
+<?php include_once("../config.php"); ?>
+<?php include_once("../connection.php"); ?>
+<?php include_once("../components/functions.php");
+
+if (isset($_GET['filter'])) {
+    $filter = filter_input(INPUT_GET, "filter", FILTER_SANITIZE_NUMBER_INT);
+} else {
+    $filter = NULL;
+}
+
+if (isset($_GET['submit'])) {
+    $ticket_id = filter_input(INPUT_GET, "ticket_id", FILTER_SANITIZE_SPECIAL_CHARS);
+    $ticket_title = filter_input(INPUT_GET, "ticket_title", FILTER_SANITIZE_SPECIAL_CHARS);
+} else {
+    $ticket_id = NULL;
+    $ticket_title = NULL;
+}
+?>
 <!DOCTYPE html>
 <html lang="nl">
+
 <head>
     <title>Bottom up - Overzicht</title>
-    <?php include_once("../components/head.html");?>
+    <?php include_once("../components/head.html"); ?>
 </head>
 
 <body>
@@ -12,14 +29,15 @@
     <?php include_once("../components/header.html") ?>
     <div id="content" class="container">
         <h1>Ticket overzicht</h1>
+        <?php var_dump($_SESSION); ?>
         <div class="row mb-3">
             <div class="col-lg-12">
                 <form action="<?= $_SERVER["PHP_SELF"] ?>" method="GET">
                     <label>Ticket id
-                        <input type="text" name="ticket_id" class="rounded form-control w-25 d-inline" name="ticket-id">
+                        <input type="text" name="ticket_id" class="rounded form-control w-25 d-inline">
                     </label>
                     <label class="ml-2">Titel
-                        <input type="text" name="ticket_title" class="rounded form-control w-25 d-inline" name="titel">
+                        <input type="text" name="ticket_title" class="rounded form-control w-25 d-inline">
                     </label>
                     <button class="btn btn-primary" name="submit" type="submit"><span class="material-icons align-middle">search</span> Zoeken</button>
                 </form>
@@ -32,79 +50,42 @@
                         <h4 class="mt-0">Status filteren:</h4>
                     </div>
                     <div class="col-lg-12">
-                        <form action="<?= $_SERVER["PHP_SELF"] ?>" method="get">
-                            <button class="btn btn-primary" name="filter" value="new" type="submit">Nieuw</button>
+                        <form action="<?= $_SERVER["PHP_SELF"] ?>" method="GET">
+                            <button class="btn btn-primary" name="filter" value="" type="submit">Alles</button>
                         </form>
                     </div>
                 </div>
                 <div class="row mb-2">
                     <div class="col-lg-12">
-                        <form action="<?= $_SERVER["PHP_SELF"] ?>" method="get">
-                            <button class="btn btn-primary" name="filter" value="transit" type="submit">In Behandeling</button>
+                        <form action="<?= $_SERVER["PHP_SELF"] ?>" method="GET">
+                            <button class="btn btn-primary" name="filter" value="1" type="submit">Nieuw</button>
                         </form>
                     </div>
                 </div>
                 <div class="row mb-2">
                     <div class="col-lg-12">
-                        <form action="<?= $_SERVER["PHP_SELF"] ?>" method="get">
-                            <button class="btn btn-primary" name="filter" value="onHold" type="submit">On hold</button>
+                        <form action="<?= $_SERVER["PHP_SELF"] ?>" method="GET">
+                            <button class="btn btn-primary" name="filter" value="2" type="submit">In Behandeling</button>
                         </form>
                     </div>
                 </div>
                 <div class="row mb-2">
                     <div class="col-lg-12">
-                        <form action="<?= $_SERVER["PHP_SELF"] ?>" method="get">
-                            <button class="btn btn-primary" name="filter" value="closed" type="submit">Gesloten</button>
+                        <form action="<?= $_SERVER["PHP_SELF"] ?>" method="GET">
+                            <button class="btn btn-primary" name="filter" value="3" type="submit">On hold</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-lg-12">
+                        <form action="<?= $_SERVER["PHP_SELF"] ?>" method="GET">
+                            <button class="btn btn-primary" name="filter" value="4" type="submit">Gesloten</button>
                         </form>
                     </div>
                 </div>
             </div>
             <div class="col-lg-10">
-                <table cellspacing="0" cellpadding="0" class="table">
-                    <thead>
-                        <tr data-href="ticket_detail.php?id=1">
-                            <th>id</th>
-                            <th>Aanmaak datum</th>
-                            <th>Titel</th>
-                            <th>Bedrijf</th>
-                            <th>Prioriteit</th>
-                            <th>Status</th>
-                            <th>Hoofdcategorie</th>
-                            <th>Categorie</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php 
-                            $stmt = mysqli_prepare($db, 
-                                    "SELECT issue_id, `created at`, title, company.name, priority, issue.`status`, category, sub_category 
-                                     FROM issue
-                                     JOIN company ON issue.company_id = company.company_id
-                                     WHERE category = 'dienst/service'");
-                            mysqli_stmt_execute($stmt) OR DIE(mysqli_error($db));
-                            mysqli_stmt_store_result($stmt) OR DIE(mysqli_error($db));
-                            mysqli_stmt_bind_result($stmt, $issueID, $createdAt, $title, $companyName, $priority, $status, $category, $subCategory);
-                        ?>
-                        <?php 
-                        if(mysqli_stmt_num_rows($stmt) > 0) {
-                            while(mysqli_stmt_fetch($stmt)) {
-                                echo
-                                "<tr class='action' data-href='ticket_detail.php?id={$issueID}'>
-                                    <td>{$issueID}</td>
-                                    <td>{$createdAt}</td>
-                                    <td>{$title}</td>
-                                    <td>{$companyName}</td>
-                                    <td>{$priority}</td>
-                                    <td>{$status}</td>
-                                    <td>{$category}</td>
-                                    <td>{$subCategory}</td>
-                                </tr>";
-                            }
-                        } else {
-                            echo"<td colspan='8'>U heeft momenteel geen nieuwe dienst/service aanvragen.</td>";
-                        }
-                        ?>                   
-                    </tbody>
-                </table>
+                <?php echo getIssueOverview($db, $_SESSION['companyId'], $_SESSION['userId'], "dienst/service", $filter, $ticket_id, $ticket_title); ?>
                 <script>
                     $(document).ready(function($) {
                         $(".action").click(function() {
