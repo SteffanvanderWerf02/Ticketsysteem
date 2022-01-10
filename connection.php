71ec264fe2b1f4d83e974551d634b70f372128de
DelanoKoster1 <delano.koster@student.nhlstenden.com>
2022-01-10 13:42:03 +0100

    merge conflict opgelost

--- connection.php
+++ connection.php
@@ -13,14 +13,9 @@ mysqli_report(MYSQLI_REPORT_STRICT);
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 try {
     // Create connection
-<<<<<<< HEAD
-    $db = new mysqli('localhost', 'root', 'root', 'bottomup');
-     //echo "You have connected to your database with MYSQLI";
-=======
     $db = new mysqli(CONNECTION, USERNAME, PASSWORD, DATABASE);
-    CheckAcces(isset($_SESSION['loggedIn']),$actual_link);
+    CheckAcces(isset($_SESSION['loggedIn']), $actual_link);
     //echo "You have connected to your database with MYSQLI";
->>>>>>> ae882a2cf878ec42029b3ad0039f4b740f045e63
 } catch (Exception $e) {
     // error connection
     $error_message = $e->getMessage();
